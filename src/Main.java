04a1fb9953f5ba7975d1c61ec8fa57abf606ed73
Suerov <suerovm2@gmail.com>
2023-06-05 19:47:36 +0300

    Commit message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        int num1 = scanner.nextInt();
+        System.out.println(method(num, num1));
+    }
 
-
+    public static  String method (String a, int b) {
+        return a.substring(b);
     }
 }
